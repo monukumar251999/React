c94341ff3e4cff72a9e0a0e0380bcbc0ef5e2ce4
monukumar251999 <59949759+monukumar251999@users.noreply.github.com>
2021-06-26 02:36:54 +0530

    Initial commit

---
+++ src/component/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDom from "react-dom";
+import Heading from "./Heading";
+import UnorderedList from "./UnorderedList";
+
+function App() {
+  return (
+    <div>
+      <Heading />
+      <UnorderedList />
+    </div>
+  );
+}
+
+export default App;
